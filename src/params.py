2c5cc9028c5776683bf671a0fe31f952bc3605d4
Alex <alexcarrerav2@gmail.com>
2025-10-13 01:01:49 +0200

    add audio handling param
    audio + music levels management, relative paths...

--- src/params.py
+++ src/params.py
@@ -14,6 +14,16 @@ WHITE = (255, 255, 255)
 fps = 60
 splitRatioScreen = 0.33 # ratio entre côté gauche et droite
 
+SFX_LEVEL =0.5
+MUSIC_LEVEL = 0.4
+
+AUDIO_DIR = "/assets/audio"
+
+MUSIC_TRACK_DIR = AUDIO_DIR +"/music" "/mainTrack.mp3" 
+
+SFX_DIR =  AUDIO_DIR + "/mainTrack.mp3" 
+
+
 def getScreenSize(screenW:int,screenH:int)->int:
     """   
     Fonction qui vérifie quelle taille (en pixels) maximale on peut avoir de sorte à avoir un ratio
