c54c49976171441ab6d5850a985bbdf895060048
Alex <alexcarrerav2@gmail.com>
2025-10-13 00:23:48 +0200

    Creation fonction getScreenSize
    Utile si on travail avec plusieurs tailles d'écrans

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ SMALL_FONT_SIZE = 10
 class Game:
     def __init__(self):
         pygame.init()
+        # permet d'ajuster l'écran afin de maximiser la taille tout en conservant un aspect ratio de 16:9
+        info = pygame.display.Info()
+        params.screenWidth, params.screenHeight = params.getScreenSize( info.current_w, info.current_h)
+        
         self.screen = pygame.display.set_mode((params.screenWidth, params.screenHeight), pygame.NOFRAME)
         pygame.display.set_caption("Blue Prince Emulation")
         self.clock = pygame.time.Clock()
@@ -24,14 +28,12 @@ class Game:
 
         # Load fonts (must be in the same folder)
         self.default_font = pygame.font.Font("assets/fonts/Helvetica.ttf", FONT_SIZE)  # general text
-
         self.special_font = pygame.font.Font("assets/fonts/damnarc.ttf", FONT_SIZE + 10)  # specific text
         self.small_font = pygame.font.Font("assets/fonts/Helvetica.ttf", SMALL_FONT_SIZE)  # smaller text
         # Placeholder texts
         self.left_text = "Left info"
         self.right_text = "Right info"
         self.special_text = "Day One"
-
 
     def draw_text(self, text, position, font=None, color=TEXT_COLOR):
         """Draw text with optional font and color."""
@@ -61,8 +63,6 @@ class Game:
         self.draw_text(fps_text, (params.screenWidth//2 - PADDING -  self.small_font.size(fps_text)[0],  params.screenHeight - PADDING),font=self.small_font)
 
         pygame.display.flip()
-
-
 
 
 
--- src/params.py
+++ src/params.py
@@ -1,11 +1,36 @@
+import math
 
 # Screen settings
+
 screenWidth = 1920
 screenHeight = 1080
 
-# Colors (R, G, B)
+
+#Colors (R, G, B)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Game settings
 fps = 60
+
+
+def getScreenSize(screenW:int,screenH:int)->int:
+    """   
+    Fonction qui vérifie quelle taille (en pixels) maximale on peut avoir de sorte à avoir un ratio
+    d'image 16:9 et minimiser la bordure
+
+    Parametres:
+    screenW:int - taille écran horizontal
+    screenH:int - taille écran verticale
+
+    Retourne:
+    w,h:int - taille finale telle que aspect ratio conservé et taille image conservée
+    """
+
+    echelle = min(screenW / 16.0, screenH / 9.0)
+    w = round(math.floor(16 * echelle))
+    h = int(math.floor(9 * echelle))
+    return(w,h)
+
+
+
