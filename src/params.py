735ab9f3845090f308a1ef08058f141d386e15af
Alex <alexcarrerav2@gmail.com>
2025-10-13 01:31:12 +0200

    add DEFAULT SCREEN SIZE param

--- src/params.py
+++ src/params.py
@@ -1,10 +1,11 @@
 import math
 
 # Screen settings
-
-screenWidth = 1920 #taille image horizontal par défaut
-screenHeight = 1080 #taille image vertical par défaut
-
+DEFAULT_SCREEN_WIDTH= 1920
+DEFAULT_SCREEN_HEIGHT = 1080
+screenWidth = DEFAULT_SCREEN_WIDTH #taille image horizontal par défaut
+screenHeight = DEFAULT_SCREEN_HEIGHT #taille image vertical par défaut
+USE_DEFAULT_SCREEN_SIZE = True
 
 #Colors (R, G, B)
 BLACK = (0, 0, 0)
@@ -37,10 +38,12 @@ def getScreenSize(screenW:int,screenH:int)->int:
     Retourne:
     w,h:int - taille finale telle que aspect ratio conservé et taille image conservée
     """
-
+    if(USE_DEFAULT_SCREEN_SIZE):
+        return(DEFAULT_SCREEN_WIDTH,DEFAULT_SCREEN_HEIGHT)
     echelle = min(screenW / 16.0, screenH / 9.0)
     w = round(math.floor(16 * echelle))
     h = int(math.floor(9 * echelle))
+
     return(w,h)
 
 
