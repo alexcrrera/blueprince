e0d7c6d44a0a991d368ae3c9af40f258d9a426ba
Alex <alexcarrerav2@gmail.com>
2025-10-13 01:30:52 +0200

    add inputs.py for input management
    pending docs

---
+++ src/inputs.py
@@ -0,0 +1,50 @@
+from src import params
+
+import pygame
+
+
+class inputHandling:
+    def __init__(self):
+        # Dictionary to track key presses
+        self.key_pressed = {}
+
+    def update(self):
+        """
+        Call this every frame to update key presses.
+        """
+        self.key_pressed = {}  # reset for this frame
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.key_pressed["QUIT"] = True
+
+            if event.type == pygame.KEYDOWN:
+                self.key_pressed[event.key] = True
+                
+                if event.key == pygame.K_m:
+                    params.PLAY_MUSIC = not params.PLAY_MUSIC
+                    if params.PLAY_MUSIC:
+                        print("Music unmuted")
+                    else:
+                        print("Music muted")
+                if event.key == pygame.F_m:
+                    
+                   
+             
+               
+                    print("F PRESSED")
+
+
+            if event.type == pygame.KEYUP:
+                self.key_pressed[event.key] = False
+
+        
+
+    def is_pressed(self, key):
+        """
+        Check if a key was pressed this frame.
+        Example: inputs.is_pressed(pygame.K_SPACE)
+        """
+        return self.key_pressed.get(key, False)
+
+
