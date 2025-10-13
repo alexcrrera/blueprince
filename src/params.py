d33ed05f783ff75371a68b5363c925723d50e22d
Alex <alexcarrerav2@gmail.com>
2025-10-13 02:16:17 +0200

    migrated getScreenSize to ui.py

--- src/params.py
+++ src/params.py
@@ -7,6 +7,9 @@ screenWidth = DEFAULT_SCREEN_WIDTH #taille image horizontal par défaut
 screenHeight = DEFAULT_SCREEN_HEIGHT #taille image vertical par défaut
 USE_DEFAULT_SCREEN_SIZE = True
 
+FULL_SCREEN = not(USE_DEFAULT_SCREEN_SIZE)
+
+
 #Colors (R, G, B)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -29,25 +32,7 @@ PLAY_MUSIC = True
 
 KEEP_RUNNING = True
 
-def getScreenSize(screenW:int,screenH:int)->int:
-    """   
-    Fonction qui vérifie quelle taille (en pixels) maximale on peut avoir de sorte à avoir un ratio
-    d'image 16:9 et minimiser la bordure
 
-    Parametres:
-    screenW:int - taille écran horizontal
-    screenH:int - taille écran verticale
+# FLAGS
 
-    Retourne:
-    w,h:int - taille finale telle que aspect ratio conservé et taille image conservée
-    """
-    if(USE_DEFAULT_SCREEN_SIZE):
-        return(DEFAULT_SCREEN_WIDTH,DEFAULT_SCREEN_HEIGHT)
-    echelle = min(screenW / 16.0, screenH / 9.0)
-    w = round(math.floor(16 * echelle))
-    h = int(math.floor(9 * echelle))
-
-    return(w,h)
-
-
-
+changeScreenSizeFlag = False
