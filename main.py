bff96c5b586703ce4aea99bd949684318ac92c9c
Alex <alexcarrerav2@gmail.com>
2025-10-14 01:15:24 +0200

    rem info call (useless)

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Game:
     def __init__(self):
         pygame.init()
         # permet d'ajuster l'écran afin de maximiser la taille tout en conservant un aspect ratio de 16:9
-        info = pygame.display.Info()
+      
         self.screenHandler = ui.HandleScreen()
         self.clock = pygame.time.Clock()
         self.textHandler = ui.HandleText(self.screenHandler.screen,self.clock)
