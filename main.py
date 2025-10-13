0f6a9b8f507ecb1c6a49857cbe6c74468530590e
Alex <alexcarrerav2@gmail.com>
2025-10-13 02:17:29 +0200

    Migrated screen management to ui.py

--- main.py
+++ main.py
@@ -22,18 +22,15 @@ class Game:
         pygame.init()
         # permet d'ajuster l'écran afin de maximiser la taille tout en conservant un aspect ratio de 16:9
         info = pygame.display.Info()
-      
-
-        params.screenWidth, = params.getScreenSize( info.current_w, info.current_h)
-        self.screenHeight = params.screenHeight 
-        self.screenWidth = params.screenWidth
+        self.screenHandler = ui.handleScreen(info.current_w, info.current_h)
         
-        self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
-        self.bg = ui.handleBackground(self.screen)
+      
+ 
+        self.bg = ui.handleBackground(self.screenHandler.screen)
         self.audio = sound.HandleSound()
         self.input = inputs.inputHandling()
 
-        pygame.display.set_caption("Blue Prince Emulation")
+        
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -50,12 +47,13 @@ class Game:
         """Draw text with optional font and color."""
         font = font or self.default_font
         rendered_text = font.render(text, True, color)
-        self.screen.blit(rendered_text, position)
+        self.screenHandler.screen.blit(rendered_text, position)
 
   
 
     def update(self):
         self.running = params.KEEP_RUNNING
+        self.screenHandler.update()
         self.input.update()
         self.bg.draw()
         self.audio.update()
