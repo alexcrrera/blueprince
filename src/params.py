f39cec3133bc10e311678e4bb6db99e65646c1bc
Alex <alexcarrerav2@gmail.com>
2025-10-18 01:42:48 +0200

    rem music at start

--- src/params.py
+++ src/params.py
@@ -25,7 +25,7 @@ MUSIC_TRACK_DIR = AUDIO_DIR +"/music" +"/mainTrack.mp3"
 
 SFX_DIR =  AUDIO_DIR + "/mainTrack.mp3" 
 
-PLAY_MUSIC_START = True #play music at start
+PLAY_MUSIC_START = False #play music at start
 
 
 
