55dcc3defa3572b2571ad192de124eeb196fbe6c
MaffiaEdoardo <edoardomaffia03@gmail.com>
2025-10-20 16:05:54 +0200

    updt room class and handle json data

--- src/room_library.py
+++ src/room_library.py
@@ -5,25 +5,47 @@ from src.room import Room  # import the class Room from room.py
 
 from src import params
 
+import random
+
 
 class RoomGrid():
     def __init__(self,data):
 
         self.data = data
-        self.mansion = [[None for _ in range(params.ROOM_GRID_SIZE_VERTICAL)] for _ in range(params.ROOM_GRID_SIZE_HORIZONTAL)]
-
+        self.grid = [[None for _ in range(params.ROOM_GRID_SIZE_VERTICAL)] for _ in range(params.ROOM_GRID_SIZE_HORIZONTAL)]
+        
         entranceHallPos = [2,8]
-        entranceHall = Room("Etrance_Hall","blue",entranceHallPos[0],entranceHallPos[1], rarity = 0, cost = 0)
-     
-        self.mansion[entranceHall.x][entranceHall.y] = entranceHall
+        etranceHallName = "Entrance_Hall"
+        entranceHall = Room(etranceHallName,"blue",entranceHallPos[0],entranceHallPos[1], rarity = 0, cost = 0)
+
+        self.grid[entranceHall.x][entranceHall.y] = entranceHall
         
 
+
+    def draw_random_rooms(self, n=3):
+        """Retourne n chambres tirées aléatoirement selon leur rareté."""
+        pool = []
+        for name, data in self.rooms_data.items():
+            # Plus la rareté est élevée, moins la pièce apparaît
+            weight = 1 / (3 ** data["rarity"])
+            pool.append((name, weight))
+        names = random.choices(
+            [name for name, _ in pool],
+            weights=[w for _, w in pool],
+            k=n
+        )
+        return [name for name in names]
+
+
+    def create_room_instance(self, name, x, y):
+        """Instancie une Room à partir du JSON."""
+        return Room(name, self.rooms_data[name], x, y)
 
     def __repr__(self):
-        
-        return "\n".join(room.__repr__() for room in self.mansion)
-        
-            
-    def update(self):   
-        self.data.manor = self.mansion
- 
+
+        return "\n".join(room.__repr__() for row in self.grid for room in row if room)
+    
+    def update(self):
+        self.data.manor = self.grid
+
+    
