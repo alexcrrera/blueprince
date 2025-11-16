a3abcdb9ab42eac0e802350ef3f2c40fee022210
Alex <alexcarrerav2@gmail.com>
2025-11-16 17:31:31 +0100

    add docstring to handler, updated variable names and added comments

--- src/handler.py
+++ src/handler.py
@@ -7,47 +7,86 @@ import time
 from collections import defaultdict
 
 
-
-
 class BaseHandler(ABC):
     """
-    Classe abstraite servant de modèle pour tous les gestionnaires (UI, son, entrée, etc.)
+    Classe abstraite servant de base commune à tous les gestionnaires du projet.
+
+    Chaque Handler :
+        - reçoit une référence à HandleData (self.data)
+        - doit implémenter la méthode update()
+        - peut être utilisé comme élément générique dans d’autres handlers
+
+    Cette classe définit l’interface minimale que doivent respecter
+    les gestionnaires d’UI, d’audio, d’entrée clavier, de grille, etc.
     """
 
     def __init__(self,data=None):
         """
-        :param data: référence à l'objet HandleData (facultatif)
+        Initialise un handler générique.
+
+        Args:
+            data: Objet HandleData contenant le contexte global du jeu.
+                  Peut être None si le handler n’a pas besoin de données globales.
         """
         self.data = data
-
 
     @abstractmethod
     def update(self):
         """
-        Méthode appelée à chaque frame.
-        Chaque gestionnaire doit implémenter sa logique de mise à jour ici.
+        Méthode appelée à chaque frame par le moteur de jeu.
+        Doit être définie par les classes filles.
         """
         pass
     
     def __repr__(self):
-        """Nom lisible pour debug."""
+        """
+        Retourne un nom lisible pour le débugging.
+
+        Returns:
+            str: Le nom de la classe entre chevrons (ex: <HandleInputs>)
+        """
         return f"<{self.__class__.__name__}>"
 
 
-
 class Handlerception(BaseHandler):
-    """Fonction principale qui appelle tous les handlers
-    
     """
+    Un gestionnaire permettant d’agréger plusieurs handlers et
+    de les mettre à jour dans un ordre déterminé.
+
+    Cette classe est utilisée comme "super-handler" capable
+    d'orchestrer plusieurs sous-composants :
+        - HandleScreen
+        - HandleGridUI
+        - HandleInputs
+        - HandleText
+        - etc.
+
+    Le nom “Handlerception” provient du fait qu’il contient des handlers
+    dans un handler. "A dream within a dream"
+    """
+   
     def __init__(self, data):
+        """
+        Initialise la liste des sous-handlers.
+
+        Args:
+            data: Référence HandleData transmise aux handlers enfants.
+        """
         super().__init__(data)
         self.handlingFunctions = list()
 
-
     def addHandler(self,handler):
+        """
+        Ajoute un handler enfant qui sera mis à jour automatiquement.
+
+        Args:
+            handler: Instance dérivée de BaseHandler.
+        """
         self.handlingFunctions.append(handler)
 
     def update(self):
-        for h in self.handlingFunctions:
-            h.update()
-   
+        """
+        Met à jour l'ensemble des handlers enfants dans l'ordre où ils ont été ajoutés.
+        """
+        for hanlder in self.handlingFunctions:
+            hanlder.update()
